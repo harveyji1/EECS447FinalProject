9df2df0b41b2ebd5d404fbc2e51a6627fc92f040
Harvey Ji <88002662+harveyji1@users.noreply.github.com>
2023-04-29 22:55:29 -0500

    Login sends you to to profile.php after success

--- customerLogin.php
+++ customerLogin.php
@@ -1,31 +1,86 @@
 <?php
-    
-	$conn = mysql_connect('mysql.eecs.ku.edu', 'h589j370', 'iegh3Cho')
-    		or die('Could not connect: ' . mysql_error());
-	echo 'Connected successfully <br>';
-	mysql_select_db('h589j370') or die('Could not select database');
-    
+
+session_start();
+$mysqli = new mysqli("mysql.eecs.ku.edu", "h589j370", "iegh3Cho", "h589j370");
+
+if ($mysqli->connect_errno) {
+  printf("Connect failed: %s\n", $mysqli->connect_error); exit();
+  echo "<h1>Fail</h1>";
+}
+
+$isSuccess=0;
+$email = $_POST["email"];
+$password = $_POST["password"];
+$querySkier = "SELECT * FROM SKIER WHERE EMAIL='$email' AND PASSWORD='$password'";
+$result = $mysqli->query($querySkier);
+
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+      $dbPassword = $row['PASSWORD'];
+      $dbEmail = $row['EMAIL'];
+      if($dbPassword == $password && $dbEmail == $email)
+      {
+          $_SESSION['name']=$row['NAME'];
+          $_SESSION['email']=$row['EMAIL'];
+          $_SESSION['dob']=$row['DOB'];
+          $_SESSION['password']=$row['PASSWORD'];
+          $isSuccess=1;
+          echo "Login Found";
+      }
+  }
+}
+else {
+  echo "0 results";
+}
+
+
+
+if ($isSuccess == 0) {
+    header("Location: customerLoginPage.php?error=Incorrect User Name or Password");
+} else 
+{
+    header("Location: customer/profile.php");
+}
+
+$mysqli->close();
+    /*session_start();
+    include "db_conn.php";
+
     $email = $_POST["email"];
     $password = $_POST["password"];
-
-    $query = "SELECT * FROM SKIER WHERE EMAIL = '$email' AND PASSWORD = '$password'";
-    $result = mysql_query($query) or die('Query failed: ' . mysql_error());
     
-    echo "Number of records: ".mysql_num_rows($result)."<br>";
-    
-    if(mysql_num_rows($result) == 0){
-        echo 'Invalid Email or Username. Please Try Again';
+    $sql = "SELECT * FROM SKIER WHERE EMAIL = '$email' AND PASSWORD = '$password'";
+    $result = mysqli_query($conn, $sql);
+    $isSuccess=0;
+    if(mysqli_num_rows($result) == 1){
+        echo "Found 1 result";
+        $row = mysqli_fetch_assoc($result);
+        echo $row['EMAIL'];
+        }
+        /*
+        if($row['email'] == $email && $row['password'] == $password){
+            echo "Logged In! Redirecting now...";
+            $_SESSION['email'] = $row['email'];
+            $_SESSION['name'] = $row['name'];
+            $_SESSION['passsword'] = $row['password'];
+            $_SESSION['ssn'] = $row['ssn'];
+            $_SESSION['dob'] = $row['dob'];
+            //header("Location:customer/profile.php");
+            //exit();
+        }
+        
+        else{
+            header("Location:customerLoginPage.php?error=Incorrect User Name or Password");
+            exit();
+        }
     }
-    
     else{
-        echo'Login Succesful!';
+        header("Location:customerLoginPage.php");
+        exit();
     }
-    
-    // Free resultset
-	mysql_free_result($result);
-
-// Close connection
-	mysql_close($link);
+    */
 
 ?> 
   
