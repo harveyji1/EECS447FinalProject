af4e76724d59a5500faae919af189a9ceb321f6d
Harvey Ji <88002662+harveyji1@users.noreply.github.com>
2023-04-30 23:11:14 -0500

    Update myTickets.php

--- customer/myTickets.php
+++ customer/myTickets.php
@@ -7,8 +7,33 @@
     if ($mysqli->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error); exit();
     echo "<h1>Connection to Database Fail</h1>";
-}   
+    }
+    ?>
+        <!DOCTYPE html>
+        <html>
+        <head>
+	        <meta charset="UTF-8">
+	        <title>My Tickets Page</title>
+            <link rel="stylesheet" href="customer.css">
+        </head>
+        <body>
+    <nav>
+        <div class="container">
+            <a href="customerFrontend.php" class="logo">JINO SKI RESORTS</a>
 
+            <div class="menu-toggle">&#9776;</div>
+
+            <ul>
+                <li><a href="myTickets.php">MY TICKETS</a></li>
+                <li><a href="buyTickets.php">BUY TICKETS</a></li>
+                <li><a href="resortsPage.php">RESORTS</a></li>
+                <li><a href="myLifts.php">MY LIFTS</a></li>
+                <li><a href="profile.php">PROFILE</a></li>
+            </ul>
+        </div>
+    </nav>
+    <main>
+<?php
 $numTicket=0;
 $skierID = $_SESSION['skierid'];
 $queryTicket = "SELECT TICKETID, TICKETS.SKIERID, NAME, PURCHASEDDATE, EXPIRATIONDATE, RESORTNAME FROM TICKETS, SKIER WHERE TICKETS.SKIERID='$skierID' AND SKIER.SKIERID=TICKETS.SKIERID";
@@ -17,14 +42,8 @@ $result = $mysqli->query($queryTicket);
 if($result->num_rows > 0) {
   // output data of each row
   ?>
-  <!DOCTYPE html>
-<html>
-<head>
-	<meta charset="UTF-8">
-	<title>My Tickets Page</title>
-    <link rel="stylesheet" href="customer.css">
-</head>
- <table class="table table-hover table-borderless" >
+<div class = "table-container">
+ <table> <!--FORMAT THIS TABLE PLEASE, The div table-container might help: its purpose is to keep the footer where its at-->
                 <thead>
                     <th scope="col" class="table-header"> Ticket ID </th>
                     <th scope="col" class="table-header"> Purchase Date </th>
@@ -46,18 +65,28 @@ if($result->num_rows > 0) {
                           <td> <?php echo $_SESSION['expirationDate']; ?> </td>
                           <td> <?php echo $_SESSION['resortName']; ?> </td>
                       </tr>
-                      </html> <?php
-      }
+                      
+                      <?php
+      } ?>
+          </table>
+          <?php
   }
 
 else{
-  echo "No Tickets found. If you believe this an error, please contact Customer Support.";
+    //FORMAT PLEASE
+  echo "<p>No Tickets found. If you believe this is an error, please contact Customer Support.<p>";
 }
-        ?>
-<?php
+
     }
     else{
             header("Location: ../customerLoginPage.php");
             exit();
     }
     ?>
+    </div>
+</main>
+    <footer>
+        <p align="left">©2023 JINO SKI RESORTS</p>
+    </footer>
+    </body>
+</html>
